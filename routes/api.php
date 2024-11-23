dd7ce4a541d4945493d064c14e0c34872dddebc7
Danny79k <belloli.danny@gmail.com>
2024-11-23 19:29:46 +0000

    añadida infraestructura Orion para el modelo centro

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Models\Centro;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 use App\Http\Controllers\Api\v1\UserController;
+use App\Http\Controllers\Api\v1\CentroController;
 
 Route::group(['as' => 'api'], function(){
     Orion::resource('users', UserController::class);
+    Orion::resource('centros', CentroController::class);
 });
