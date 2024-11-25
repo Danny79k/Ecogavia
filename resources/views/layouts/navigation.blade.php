9255187a867fdcf269626f97bb33440c4790a51c
Danny79k <belloli.danny@gmail.com>
2024-11-25 14:18:35 +0000

    creada la user policy del administrador del crud, creado el link para que solo los usuarios con permisos admin puedan acceder a la vista del crud de usuarios

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @can('administrate', App\Models\User::class)
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Usuarios') }}  
+                    </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
