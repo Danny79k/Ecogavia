55823c28c535ef6b2023ff2df631075e9a5bdbb8
Danny79k <belloli.danny@gmail.com>
2024-11-25 03:03:38 +0000

    trabjando en el front, realizados cambios en el index

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div>
+        <navbar class="flex p-5 bg-slate-800 text-white font-sans">
+            <a href="" class="px-5">lorem</a>
+            <a href="" class="px-5">lorem</a>
+            <a href="" class="px-5">lorem</a>
+        </navbar>
+    </div>
+</x-app-layout>
