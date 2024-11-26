9b8f4bf7f6e9cf1408dd8817a50012131a627340
Fabio Acosta <Fabio@Acosta.com>
2024-11-26 22:51:18 +0000

    error auth solucionado

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -6,7 +6,6 @@ use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Auth;
 
 class UserController extends Controller
 {
@@ -16,9 +15,4 @@ class UserController extends Controller
     /*
     @return \Illuminate\Contracts\Auth\Authenticatable|null
     */
-
-   public function resolveUser()
-   {
-       return Auth::guard('sanctum')->user();
-   }
 }
