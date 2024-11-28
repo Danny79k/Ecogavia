261d9d4ab69945d31b8a42bcf65a9de9845fa048
Danny79k <belloli.danny@gmail.com>
2024-11-28 01:37:25 +0000

    empezando a trabajar en las vistas de la spa, hemos modificado el crud.js para empezar a mostrar los datos con los registros

--- resources/js/crud.js
+++ resources/js/crud.js
@@ -5,9 +5,9 @@ console.log("hola estoy dentro de vite");
 const main = document.querySelector("#main")
 
 
-async function getUsers() {
+async function getRegistros() {
     try {
-        let response = await fetch('http://localhost:8000/api/users')
+        let response = await fetch('http://localhost:8000/api/registros')
         if (!response.ok) {
             console.log("no se ha podido procesar la peticion");
         } else {
@@ -18,11 +18,11 @@ async function getUsers() {
     }
 }
 
-let crudButton = document.querySelector("#crudButton")
+let registros = document.querySelector("#registros")
 
-crudButton.addEventListener('click', async () => {
+registros.addEventListener('click', async () => {
     main.innerHTML = ''
-    let usuarios = await getUsers()
+    let usuarios = await getRegistros()
     let table = document.createElement("table")
     let thead = document.createElement("thead")
     let trColumna = document.createElement("tr")
@@ -33,13 +33,12 @@ crudButton.addEventListener('click', async () => {
     let thCentro = document.createElement("th")
     let thCreado = document.createElement("th")
     let thActualizado = document.createElement("th")
-    
-    main.innerHTML = `<p class=''>hola</p>`
+
     thId.innerHTML = "id"
-    thNombre.innerHTML = "nombre"
-    thACorreo.innerHTML = "correo"
-    thAdmin.innerHTML = "admin"
-    thCentro.innerHTML = "centro"
+    thNombre.innerHTML = "usuario"
+    thACorreo.innerHTML = "compostera"
+    thAdmin.innerHTML = "ciclo"
+    thCentro.innerHTML = "inicio ciclo"
     thCreado.innerHTML = "creado"
     thActualizado.innerHTML = "actualizado"
 
@@ -57,36 +56,63 @@ crudButton.addEventListener('click', async () => {
 
     let tBody = document.createElement("tbody")
 
-    table.classList.add("text-white", "bg-black", "table-auto", "text-center", "border-separate", "border-spacing-2", "border")
+    table.classList.add("text-white", "bg-gray-500", "text-center", "border-separate", "border-spacing-2", "border")
     console.log(usuarios.data);
     for (let i = 0; i < usuarios.data.length; i++) {
+
         let row = document.createElement("tr")
-        let celdaId = document.createElement("td")
-        let celdaNombre = document.createElement("td")
-        let celdaCorreo = document.createElement("td")
-        let celdaAdmin = document.createElement("td")
-        let celdaCentro = document.createElement("td")
-        let celdaCreado = document.createElement("td")
-        let celdaActualizado = document.createElement("td")
 
-        celdaId.textContent = usuarios.data[i].id
-        celdaNombre.textContent = usuarios.data[i].name
-        celdaCorreo.textContent = usuarios.data[i].email
-        celdaAdmin.textContent = usuarios.data[i].admin
-        celdaCentro.textContent = usuarios.data[i].centro_id
-        celdaCreado.textContent = usuarios.data[i].created_at
-        celdaActualizado.textContent = usuarios.data[i].updated_at
+        let celdaId = document.createElement("td");
+        celdaId.classList.add("celdaId");
+        let celdaUsuario = document.createElement("td");
+        celdaUsuario.classList.add("celdaUsuario");
+        let celdaCompostera = document.createElement("td");
+        celdaCompostera.classList.add("celdaCompostera");
+        let celdaCiclo = document.createElement("td");
+        celdaCiclo.classList.add("celdaCiclo");
+        let celdaInicioCiclo = document.createElement("td");
+        celdaInicioCiclo.classList.add("celdaInicioCiclo");
+        let celdaCreado = document.createElement("td");
+        celdaCreado.classList.add("celdaCreado");
+        let celdaActualizado = document.createElement("td");
+        celdaActualizado.classList.add("celdaActualizado");
 
-        row.appendChild(celdaId)
-        row.appendChild(celdaNombre)
-        row.appendChild(celdaCorreo)
-        row.appendChild(celdaAdmin)
-        row.appendChild(celdaCentro)
-        row.appendChild(celdaCreado)
-        row.appendChild(celdaActualizado)
+        celdaId.textContent = usuarios.data[i].id;
+        celdaUsuario.textContent = usuarios.data[i].user_id;
+        celdaCompostera.textContent = usuarios.data[i].compostera_id;
+        celdaCiclo.textContent = usuarios.data[i].ciclo_id;
+        celdaInicioCiclo.textContent = usuarios.data[i].inicio_ciclo;
+        celdaCreado.textContent = usuarios.data[i].created_at.substr(0, 9);
+        celdaActualizado.textContent = usuarios.data[i].updated_at.substr(0, 9);
+
+
+        row.appendChild(celdaId);
+        row.appendChild(celdaUsuario);
+        row.appendChild(celdaCompostera);
+        row.appendChild(celdaCiclo);
+        row.appendChild(celdaInicioCiclo);
+        row.appendChild(celdaCreado);
+        row.appendChild(celdaActualizado);
 
         tBody.appendChild(row)
+
+
+
     }
     table.appendChild(tBody)
     main.appendChild(table)
+    main.classList.add('flex', 'justify-center')
+
+    // haciendo click en los apartados por separado
+
+    if (document.querySelectorAll('.celdaId').length >= 1) {
+        let ids = document.querySelectorAll('.celdaId')
+
+        ids.forEach(celda => {
+            celda.addEventListener('click', (e) => {
+                let id = e.target.textContent;
+                console.log(id);
+            })
+        })
+    }
 })
